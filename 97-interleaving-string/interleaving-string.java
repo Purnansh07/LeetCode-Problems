66e852b5ecb94070f44ceefa1e1d3cd8ace1dc57
Purnansh <87643664+Purnansh07@users.noreply.github.com>
2026-08-14 00:00:29 +0530

    Time: 6 ms (28.88%) | Memory: 43.3 MB (38.91%) - LeetSync

---
+++ 97-interleaving-string/interleaving-string.java
@@ -0,0 +1,56 @@
+class Solution {
+    public boolean isInterleave(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+
+        if (m + n != s3.length()) {
+            return false;
+        }
+
+        // Use the shorter string for the DP array
+        if (n > m) {
+            return solve(s1, s2, s3);
+        }
+
+        return solve(s2, s1, s3);
+    }
+
+    private boolean solve(String s1, String s2, String s3) {
+        int m = s1.length();
+        int n = s2.length();
+
+        boolean[] dp = new boolean[n + 1];
+
+        dp[0] = true;
+
+        // Using only s2
+        for (int j = 1; j <= n; j++) {
+            dp[j] = dp[j - 1]
+                    && s2.charAt(j - 1) == s3.charAt(j - 1);
+        }
+
+        for (int i = 1; i <= m; i++) {
+
+            // Using only s1
+            dp[0] = dp[0]
+                    && s1.charAt(i - 1) == s3.charAt(i - 1);
+
+            for (int j = 1; j <= n; j++) {
+
+                char current = s3.charAt(i + j - 1);
+
+                boolean fromS1 =
+                        dp[j] &&
+                        s1.charAt(i - 1) == current;
+
+                boolean fromS2 =
+                        dp[j - 1] &&
+                        s2.charAt(j - 1) == current;
+
+                dp[j] = fromS1 || fromS2;
+            }
+        }
+
+        return dp[n];
+    }
+}
