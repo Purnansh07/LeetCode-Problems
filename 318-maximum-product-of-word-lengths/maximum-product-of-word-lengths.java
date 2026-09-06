89d776a3a65123d7a7d62526e260c4fb713276d3
Purnansh <87643664+Purnansh07@users.noreply.github.com>
2026-09-06 23:37:10 +0530

    Time: 9 ms (99.56%) | Memory: 49.5 MB (45.94%) - LeetSync

---
+++ 318-maximum-product-of-word-lengths/maximum-product-of-word-lengths.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int maxProduct(String[] words) {
+        int n = words.length;
+        int[] mask = new int[n];
+
+        // Build bitmask for every word
+        for (int i = 0; i < n; i++) {
+            for (char c : words[i].toCharArray()) {
+                mask[i] |= 1 << (c - 'a');
+            }
+        }
+
+        int ans = 0;
+
+        // Check every pair
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+
+                // No common characters
+                if ((mask[i] & mask[j]) == 0) {
+                    ans = Math.max(
+                        ans,
+                        words[i].length() * words[j].length()
+                    );
+                }
+            }
+        }
+
+        return ans;
+    }
+}
